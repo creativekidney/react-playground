4f2ae4a138a2e07be00c1a62027bdf27e12f94b0
Luke Kidney <Luke.Kidney@bartleboglehegarty.com>
2016-11-05 21:10:24 +0000

    webpack added

---
+++ frontend/webpack.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const webpack = require("webpack");
+
+module.exports = {
+  context: __dirname + "/src",
+  entry: {
+    app: "./app.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist/assets",
+    publicPath: "/assets",            // New
+  },
+  devServer: {
+    contentBase: __dirname + "/src",  // New
+  },
+};
